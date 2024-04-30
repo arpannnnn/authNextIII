590926903624f8fab35a2f8d795fea7456d79bc8
Arpan <rockarpan31@gmail.com>
2024-04-30 21:00:10 +0545

    authNext

--- components/login.jsx
+++ components/login.jsx
@@ -27,7 +27,7 @@ export default function CustomLogin() {
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                         <img className="w-8 h-8 mr-2" src="https://images.unsplash.com/photo-1714386002041-06e84d3633f7?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="logo" />
-                        Next-Auth
+                        Next-Authhhh
                     </a>
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
